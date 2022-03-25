5a79a3f8aff353810625333a7ecf2be652b69e9c
Emmanuelle Esguerra <emman.esguerra2013@gmail.com>
2022-03-25 16:12:24 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>AE AXIE</title>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/vue@2/dist/vue.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/lodash@4.17.21/lodash.min.js"></script>
+
+        <link rel="stylesheet" href="css/style.css">
+    </head>
+    <body>
+        <div class="container">
+            <div class="wrapper">
+                <div class="row">
+                    <div class="col-12 text-center">
+                        <h1>AE AXIE DAMAGE CALCULATOR</h1>
+                    </div>
+                </div>
+                <div class="row">
+                    <div id="app" class="col-12" style="position: relative">
+                        <div class="row">
+                            <div class="col-12" v-for="axie in axies">
+                                <div v-bind:class="loadClass(skills)" v-for="skills in axie.skills" v-bind:style="loadBg(skills.img)" @click="selectSkills(skills)">
+                                    <span class="ene">{{ skills.ene }}</span>
+                                    <span class="name">{{ skills.name }}</span>
+
+                                    <p class="desc">{{ skills.desc }}</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="calc">
+                            <h4>Target Class</h4>
+                            <div class="form-check" v-for="target in targettype">
+                                <input type="radio" class="form-check-input" id="radio1" name="optradio" value="option1" checked>
+                                <label class="form-check-label" for="radio1">{{ target }}</label>
+                            </div>
+                            <h4 class="mt-3">Selected Skills</h4>
+                            <div v-for="sskills in selectedskills">
+                                <label class="form-check-label">{{ sskills.name }}</label>
+                            </div>
+                            
+                            <h4 class="mt-3">Total Damage</h4>
+                            <h2><strong>{{ totalDamage }}</strong></h2>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <script src="js/script.js"></script>
+    </body>
+</html>
---
+++ js/script.js
@@ -0,0 +1,182 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+var app = new Vue({
+    el: '#app',
+    data: {
+        targettype: [
+            'Plant/ Reptile/ Dusk',
+            'Beat/ Mech/ Bug',
+            'Bird/ Aqua/ Dawn'
+        ],
+        selectedskills: [],
+        axies: [
+            {
+                name: 'Mech',
+                skills: [
+                    {
+                        name: 'Heroic Reward',
+                        ene: 1,
+                        atk: 55,
+                        def: 0,
+                        desc: 'Draw a card when attacking an Aquatic, Bird, or Dawn target.',
+                        img: 'beast-back-04.png',
+                        selected: false
+                    },
+                    {
+                        name: 'Nut Crack',
+                        ene: 1,
+                        atk: 105,
+                        def: 30,
+                        desc: "Deal 120% damage when comboed with another 'Nut Cracker' card.",
+                        img: 'beast-mouth-02.png',
+                        selected: false
+                    },
+                    {
+                        name: 'Sinister Strike',
+                        ene: 1,
+                        atk: 130,
+                        def: 20,
+                        desc: "Deal 200% damage on critical strikes.",
+                        img: 'beast-horn-10.png',
+                        selected: false
+                    },
+                    {
+                        name: 'Nut Throw',
+                        ene: 1,
+                        atk: 105,
+                        def: 30,
+                        desc: "Deal 120% damage when comboed with another 'Nut Cracker' card.",
+                        img: 'beast-tail-10.png',
+                        selected: false
+                    },
+                ]
+            },
+            {
+                name: 'Dusk',
+                skills: [
+                    {
+                        name: 'Vine Dagger',
+                        ene: 0,
+                        atk: 20,
+                        def: 30,
+                        desc: 'Double shield from this card when comboed with a plant card.',
+                        img: 'reptile-back-06.png',
+                        selected: false
+                    },
+                    {
+                        name: 'Chomp',
+                        ene: 1,
+                        atk: 75,
+                        def: 50,
+                        desc: 'Apply Stun to enemy when comboed with at least 2 additional cards.',
+                        img: 'reptile-mouth-10.png',
+                        selected: false
+                    },
+                    {
+                        name: 'Headshot',
+                        ene: 1,
+                        atk: 130,
+                        def: 0,
+                        desc: "Disable target's horn cards next round.",
+                        img: 'bird-horn-08.png',
+                        selected: false
+                    },
+                    {
+                        name: 'Spicy Surprise',
+                        ene: 1,
+                        atk: 80,
+                        def: 50,
+                        desc: "Disable target's mouth cards next round.",
+                        img: 'plant-tail-12.png',
+                        selected: false
+                    }
+                ]
+            },
+            {
+                name: 'Plant',
+                skills: [
+                    {
+                        name: 'Bug Splat',
+                        ene: 1,
+                        atk: 110,
+                        def: 50,
+                        desc: "Deal 50% more damage when attacking Bug targets.",
+                        img: 'bug-back-08.png',
+                        selected: false
+                    },
+                    {
+                        name: 'Vegetal Bite',
+                        ene: 1,
+                        atk: 30,
+                        def: 30,
+                        desc: "Steal 1 energy from your opponent when comboed with another card.",
+                        img: 'plant-mouth-02.png',
+                        selected: false
+                    },
+                    {
+                        name: 'Prickly Trap',
+                        ene: 1,
+                        atk: 110,
+                        def: 20,
+                        desc: "Deal 120% damage if this Axie attacks last.",
+                        img: 'plant-horn-10.png',
+                        selected: false
+                    },
+                    {
+                        name: 'Spicy Surprise',
+                        ene: 1,
+                        atk: 80,
+                        def: 50,
+                        desc: "Disable target's mouth cards next round.",
+                        img: 'plant-tail-12.png',
+                        selected: false
+                    },
+                ]
+            },
+        ],
+        message: 'Hello Vue!'
+    },
+    computed: {
+        totalDamage: function () {
+            var count = 0;
+            _.forEach(this.selectedskills, function (skills) {
+                count += skills.atk;
+            });
+            
+            return count;
+        }
+    },
+    methods: {
+        loadBg: function (image) {
+            return "background-image: url(images/" + image + ")";
+        },
+        loadClass: function (skill) {
+            if (skill.selected) {
+                return "dcards active";
+            } else {
+                return "dcards";
+            }
+        },
+        selectSkills: function (skill) {
+            skill.selected = !skill.selected;
+
+            var valObj = this.selectedskills.findIndex(object => {
+                return object.name === skill.name;
+            });
+            console.log(valObj);
+
+            if (skill.selected) {
+                if (valObj < 0) {
+                    this.selectedskills.push(skill);
+                }
+            } else {
+                if (valObj >= 0) {
+                    this.selectedskills.splice(valObj, 1);
+                }
+            }
+        }
+    }
+})
